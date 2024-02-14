9c3028b26a11387a9f846f652949993c9871fcc2
ThomasRavetto <travetto18@gmail.com>
2024-02-14 18:24:22 +0100

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,42 @@ import { BrowserRouter, Routes, Route, Navigate, } from "react-router-dom";
 import './App.css';
 import LoginForm from './components/auth/LoginForm';
 import RegisterForm from './components/auth/RegisterForm';
+import Dashboard from './components/dashboard/Dashboard';
+
+const SERVER_URL = window.location.origin;
+
+const API_URL = window.location.origin + '/v1';
 
 function App() {
+
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [userId, setId] = useState();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [sessionChecked, setSessionChecked] = useState(false);
+
+  function loadUser (user, callback) {
+    setId(user.id);
+    setUsername(user.username)
+    setEmail(user.email)
+    setAuthenticated(true);
+    if (callback) {
+      callback();
+    }
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path='/auth/login' element= {
-            <LoginForm/>
+            <LoginForm loadUser={loadUser} API_URL={API_URL}/>
           }/>
           <Route path='/auth/register' element= {
-            <RegisterForm/>
+            <RegisterForm loadUser={loadUser} API_URL={API_URL}/>
+          }/>
+          <Route path='/' element= {
+            <Dashboard username={username} email={email}/>
           }/>
         </Routes>
       </BrowserRouter>
