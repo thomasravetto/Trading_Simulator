3354c124c092244aa595b8fa19bf01c9e92cc382
ThomasRavetto <travetto18@gmail.com>
2024-04-10 18:16:47 +0200

    update to Operations component

--- client/src/components/market/OperationWindow.js
+++ client/src/components/market/OperationWindow.js
@@ -8,6 +8,8 @@ const { user_id, asset_symbol, asset_name, asset_quantity, API_URL } = props;
     const [quantity, setQuantity] = useState(1);
     const [buying_operation, setBuyingOperation] = useState(true);
     const [component_visible, setComponentVisible] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState('');
 
     async function getLatestData (asset_symbol) {
         const resp = await fetch(API_URL + '/market/get_latest_data', {
@@ -57,13 +59,15 @@ const { user_id, asset_symbol, asset_name, asset_quantity, API_URL } = props;
                     })
                 });
 
-                data = await resp.json();
+                if (resp.ok) {
+                    setSuccess(true);
+                    setError('');
+                } else {
+                    data = await resp.json();
+                    setError(data.error);
+                }
 
             } else if (operation === 'sell') {
-
-                if (parseFloat(asset_quantity) < parseFloat(quantity)) {
-                    throw new Error('Insufficient quantity to sell');
-                }
 
                 const resp = await fetch(API_URL + '/portfolio/sell_asset', {
                     method: 'post',
@@ -77,7 +81,14 @@ const { user_id, asset_symbol, asset_name, asset_quantity, API_URL } = props;
                     })
                 });
 
-                data = await resp.json();
+                if (resp.ok) {
+                    setSuccess(true);
+                    setError('');
+                } else {
+                    data = await resp.json();
+                    setError(data.error);
+                }
+
             }
         } catch (error) {
             console.error(error);
@@ -114,36 +125,68 @@ const { user_id, asset_symbol, asset_name, asset_quantity, API_URL } = props;
     })
 
     return (
-        <div className={`operations_background_cover ${!component_visible ? "operations_window_hidden" : ""}`}>
-            <div className={`operations_window_container`}>
-                <div className="operations_window">
-                    <div className={`operations_asset_info ${buying_operation ? 'buy' : 'sell'}`}>
-                        <div>
-                            <h1>{asset_symbol} </h1>
-                            <p>{asset_name}</p>
+        <div>
+            {
+                !success ?
+                <div className={`operations_background_cover ${!component_visible ? "operations_window_hidden" : ""}`}>
+                    <div className={`operations_window_container`}>
+                        <div className="operations_window">
+                            <div className={`operations_asset_info ${buying_operation ? 'buy' : 'sell'}`}>
+                                <div>
+                                    <h1>{asset_symbol} </h1>
+                                    <p>{asset_name}</p>
+                                </div>
+                                <button className="close_button" onClick={() => {
+                                    setComponentVisibility(false);
+                                    setSuccess(false);
+                                    setError('');
+                                }}>&#215;</button>
+                                <h2>{price.slice(0, -2)}$</h2>
+                            </div>
+                            <div className="operations_buysell_container">
+                                <button className="buy" onClick={() => {
+                                    handleSetBuyingOperation(true);
+                                    setError('');}}>BUY</button>
+                                <button className="sell" onClick={() => {
+                                    handleSetBuyingOperation(false);
+                                    setError('');}}>SELL</button>
+                            </div>
                         </div>
-                        <button className="close_button" onClick={() => setComponentVisibility(false)}>&#215;</button>
-                        <h2>{price.slice(0, -2)}$</h2>
+                        <div className="operations_input_container">
+                            <input type="number" placeholder="Quantity" value={operation_value} onChange={(e) => setOperationValue(e.target.value >= 0 ? parseFloat(e.target.value) : 0)}></input>
+                            <span>$</span>
+                        </div>
+                        <div className="operation_message">
+                            <p>You are {buying_operation ? 'buying' : 'selling'}:</p>
+                            <p>{quantity} Shares</p>
+                        </div>
+                        {
+                            error ?
+                            <div className="operations_error_container">{error}</div> :
+                            <div></div>
+                        }
+                        {
+                            buying_operation ?
+                            <button className="operation_button buy" onClick={() => handleTransaction(user_id, asset_symbol, asset_name, price, quantity, 'buy')}>BUY</button> :
+                            <button className="operation_button sell" onClick={() => handleTransaction(user_id, asset_symbol, asset_name, price, quantity, 'sell')}>SELL</button>
+                        }
                     </div>
-                    <div className="operations_buysell_container">
-                        <button className="buy" onClick={() => handleSetBuyingOperation(true)}>BUY</button>
-                        <button className="sell" onClick={() => handleSetBuyingOperation(false)}>SELL</button>
+                </div> :
+                <div className={!component_visible ? "operations_window_hidden" : ""}>
+                    <div className="operations_success_background">
+                    <button className="close_button_success" onClick={() => {
+                                    setComponentVisibility(false);
+                                    setSuccess(false);
+                                    setError('');
+                                }}>&#215;</button>
+                        <div style={{textAlign: 'center'}}>
+                            <p>You succesfully {buying_operation ? 'bought' : 'sold'}:</p>
+                            <p>{quantity} shares!</p>
+                        </div>
+                        <button className={buying_operation ? 'operate_again_button buy_again' : 'operate_again_button sell_again'} onClick={() => setSuccess(false)}>{buying_operation ? 'BUY AGAIN' : 'SELL AGAIN'}</button>
                     </div>
                 </div>
-                <div className="operations_input_container">
-                    <input type="number" placeholder="Quantity" value={operation_value} onChange={(e) => setOperationValue(e.target.value >= 0 ? parseFloat(e.target.value) : 0)}></input>
-                    <span>$</span>
-                </div>
-                <div className="operation_message">
-                    <p>You are {buying_operation ? 'buying' : 'selling'}:</p>
-                    <p>{quantity} Shares</p>
-                </div>
-                {
-                    buying_operation ?
-                    <button className="operation_button buy" onClick={() => handleTransaction(user_id, asset_symbol, asset_name, price, quantity, 'buy')}>BUY</button> :
-                    <button className="operation_button sell" onClick={() => handleTransaction(user_id, asset_symbol, asset_name, price, quantity, 'sell')}>SELL</button>
-                }
-            </div>
+            }
         </div>
     )
 });
